920a5fcf706fc3688c328058eb6573a8e874fb39
yusufom <yusufolamilekanmuktar@gmail.com>
2022-04-28 18:17:42 +0100

    init

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,9 +1,23 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    container: {
-      center: true,
-      padding: ''
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extends:{
+      colors:{
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      }
     },
   },
   plugins: [],
